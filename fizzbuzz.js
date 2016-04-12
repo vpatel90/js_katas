ac6cb457ee7ac77f1c76d7e747653c3dde208724
Vivek Patel <vnp229@gmail.com>
2016-04-12 13:32:57 -0500

    added buzz to js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,9 +3,12 @@ var fizzBuzz = function (n) {
     console.log("fizzbuzz");
   } else if ( n % 3 === 0 ) {
     console.log("fizz");
+  } else if ( n % 5 === 0 ) {
+    console.log("buzz");
   }
 
 };
 
 fizzBuzz(15);
 fizzBuzz(3);
+fizzBuzz(5);
